ae7719aba8150139bc39a27db2cbc31353c094fd
Goldie <goldiejackson25@yahoo.com>
2021-10-01 07:41:12 -0500

    commit tree e9f9dcb0accce3100cdc4a557fe1d60d448ed798

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
+const x = [3,8,12,4,11,7]
+const y = 10
+
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  debugger;
+  const newArray = array.filter(e => e <= target);
+  for(let i = 0; i < newArray.length; i++){  //i = 0
+    const thisArray = newArray.slice(i); // thisarray = 8 4 7
+    console.log(newArray)
+    console.log(thisArray)
+    for(const el in thisArray){
+      if(newArray[i] + el === target)
+      return true;
+    }
+  }
+  return false;
 }
+
+
+
+
 
 /* 
   Write the Big O time complexity of your function here
 */
 
 /* 
-  Add your pseudocode here
+The function should return true if any pair of numbers in the array adds up to the target number.
+
+get numbers from array less than the target number and set to new array
+get first el of array
+first number plus iteration
 */
 
 /*
-  Add written explanation of your solution here
+  [3, 8, 12, 4, 11, 7], 10)
+  new array = 3 , 8, 4, 7
+  i=0
+  3 + 8
+ 3 + 4
+ 3 + 7 
+
+
+
 */
 
 // You can run `node index.js` to view these console logs
